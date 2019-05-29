d818e0c2ac572cfa0e307ed92eec729cb68ad345
Budi Erwanto <budi@call-levels.com>
2019-05-29 11:35:51 +0700

    Add and Apply redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Route, BrowserRouter as Router} from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import reducers from './reducers/';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const wrapComponent = (
     <Router>
         <Route exact path="/" component={App}/>
     </Router>
 )
-ReactDOM.render(wrapComponent, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        {wrapComponent}
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
