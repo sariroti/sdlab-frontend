c7c66749b32bdccd0c24d0275a869930921d83ed
Budi Erwanto <budi@call-levels.com>
2019-05-30 17:22:38 +0700

    Add Profile Component

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,13 @@ import {
         FORGOT_PASSWORD_VERIFY_TOKEN_URL
     } from '../constants/url';
 
+const config = {
+    headers: {
+        Authorization:'',
+        crossDomain: true
+    }
+}
+
 export const login = (user) => async dispatch => {
     const response = await axios.post(LOGIN_URL, user);
     if(response.data.error){
@@ -19,7 +26,6 @@ export const login = (user) => async dispatch => {
 export const register = (user) => async dispatch => {
 
     const response = await axios.post(USER_URL, qs.stringify(user));
-    console.log(response.data.payload);
     dispatch({type:'REGISTER', payload:response.data.payload});
 }
 
@@ -58,6 +64,18 @@ const changePasswordDispatch = (error, data) => {
     }
     return payload;
 }
+
+const updateProfileDispatch = (error, data) => {
+    const payload = 
+        {
+            type:'UPDATE_PROFILE',
+            payload:{
+                data
+            },
+            error
+    }
+    return payload;
+}
 export const verifyTokenPasswordChange = (token) => async dispatch => {
         const response = await axios.post(FORGOT_PASSWORD_VERIFY_TOKEN_URL, qs.stringify({token}));
        
@@ -66,5 +84,19 @@ export const verifyTokenPasswordChange = (token) => async dispatch => {
 export const changePassword = (user) => async dispatch => {
     const response = await axios.post(FORGOT_PASSWORD_CHANGE_URL, qs.stringify({token:user.token, password:user.password}));
     console.log(response)
+    dispatch(changePasswordDispatch(response.data.error, response.data.payload));
+}
+
+export const updateProfile = (user, token) => async dispatch => {
+    config.headers.Authorization ='bearer ' + token;
+    const response = await axios.put(USER_URL, qs.stringify(user), config);
+    console.log(response)
+    dispatch(updateProfileDispatch(response.data.error, response.data.payload));
+}
+
+export const getProfile = (token) => async dispatch => {
+    config.headers.Authorization ='bearer ' + token;
+    const response = await axios.get(USER_URL,config);
+    
     dispatch(changePasswordDispatch(response.data.error, response.data.payload));
 }
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -10,6 +10,10 @@ export default (state = {}, action) => {
           return {...state, payload:action.payload}; 
       case 'PASSWORD_CHANGE':
           return {...state, payload:action.payload};
+      case 'GET_PROFILE':
+          return {...state, payload:action.payload};
+      case 'UPDATE_PROFILE':
+          return {...state, payload:action.payload};
       default:
         return state;
     }
